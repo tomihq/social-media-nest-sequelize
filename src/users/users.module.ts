acb3cbc9c16e72f8aa31c7165ef02b7650ee681c
Tomi <hernandeztomas584@gmail.com>
2024-01-25 09:43:02 -0300

    AuthModule import in users module to use JWT validations

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   imports: [
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+    AuthModule
   ],
   providers: [UsersService],
 })
