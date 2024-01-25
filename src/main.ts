62500d1611d7ee6e32e097e98d02d7d9713c9b69
Tomi <hernandeztomas584@gmail.com>
2024-01-25 09:38:05 -0300

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.use(helmet())
+
   await app.listen(3000);
 }
 bootstrap();
