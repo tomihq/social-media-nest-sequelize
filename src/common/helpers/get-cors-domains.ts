4e8605ccd93cb1222ea4fb7849731d841875a88a
Tomi <hernandeztomas584@gmail.com>
2024-01-29 12:14:00 -0300

    JSDOC in getCorsDomains

--- src/common/helpers/get-cors-domains.ts
+++ src/common/helpers/get-cors-domains.ts
@@ -1,4 +1,8 @@
-export const getCorsDomains = () => {
+/**
+ * Devuelve la lista de dominios permitidos que le puedan mandar peticiones para poder configurarlos en el CORS.
+ * @returns string[]
+ */
+export const getCorsDomains = (): string[] => {
   const domains: string = process.env.CORS_DOMAINS_ALLOWED;
   const domainsArr: string[] = domains.trim().replaceAll(' ', '').split(',');
 
