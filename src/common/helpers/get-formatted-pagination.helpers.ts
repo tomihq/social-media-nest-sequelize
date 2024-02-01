f2a11932d488cee62c3607f2ad5281bacc55a901
Tomás Hernández <hernandeztomas584@gmail.com>
2024-01-31 23:38:20 -0300

    fix get users pagination

--- src/common/helpers/get-formatted-pagination.helpers.ts
+++ src/common/helpers/get-formatted-pagination.helpers.ts
@@ -3,7 +3,7 @@ import { PaginationDto } from '../dto/pagination.dto';
 export const getFormattedPagination = (
   paginationDto: PaginationDto,
 ): { skip: number; take: number; end: number } => {
-  const { page, limit: take } = paginationDto;
+  const { page = 1, limit: take = 100 } = paginationDto;
   const skip = (page - 1) * take;
   const end = skip + take;
 
